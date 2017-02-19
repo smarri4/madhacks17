fde00744fbc9866ea329a7fdd863f5120564cc83
vjagad3 <vjagad3@uic.edu>
2017-02-18 18:41:01 -0600

    Add files via upload

--- Enterprise.py
+++ Enterprise.py
@@ -11,7 +11,76 @@ class Enterprise():
         response = requests.get(url)
         return response.json()
 
-    def getAccount(self,id):
+    def getAccountbyID(self,id):
         url=self.url+'/enterprise/accounts/'+id+'?'+self.key
         response = requests.get(url)
         return response.json()
+
+    def getAllBills(self):
+        url=self.url+'/enterprise/accounts'+'?'+'key='+self.key
+        response = requests.get(url)
+        return response.json()
+
+    def getBillByID(self,id):
+        url=self.url+'/enterprise/accounts/'+id+'?'+self.key
+        response = requests.get(url)
+        return response.json()
+
+    def getAllCustomers(self):
+        url=self.url+'/enterprise/accounts'+'?'+'key='+self.key
+        response = requests.get(url)
+        return response.json()
+
+    def getCustomerByID(self,id):
+        url=self.url+'/enterprise/accounts/'+id+'?'+self.key
+        response = requests.get(url)
+        return response.json()
+
+    def getAllDeposits(self):
+        url=self.url+'/enterprise/accounts'+'?'+'key='+self.key
+        response = requests.get(url)
+        return response.json()
+
+    def getDepositByID(self,id):
+        url=self.url+'/enterprise/accounts/'+id+'?'+self.key
+        response = requests.get(url)
+        return response.json()
+
+
+    def getAllMerchants(self):
+        url=self.url+'/enterprise/accounts'+'?'+'key='+self.key
+        response = requests.get(url)
+        return response.json()
+
+    def getMerchantByID(self,id):
+        url=self.url+'/enterprise/accounts/'+id+'?'+self.key
+        response = requests.get(url)
+        return response.json()
+
+    def getAllTransfers(self):
+        url=self.url+'/enterprise/accounts'+'?'+'key='+self.key
+        response = requests.get(url)
+        return response.json()
+
+    def getTransferByID(self,id):
+        url=self.url+'/enterprise/accounts/'+id+'?'+self.key
+        response = requests.get(url)
+        return response.json()
+
+    def getAllWithdrawals(self):
+        url=self.url+'/enterprise/accounts'+'?'+'key='+self.key
+        response = requests.get(url)
+        return response.json()
+
+    def getWithdrawlsByID(self,id):
+        url=self.url+'/enterprise/accounts/'+id+'?'+self.key
+        response = requests.get(url)
+        return response.json()
+
+
+
+
+
+
+
+
